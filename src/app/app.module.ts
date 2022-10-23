4d36d6837d59ce013b8138a36b856d07baca7799
Murtuza <murtuza.shaikh@hbwsl.com>
2022-10-24 02:42:11 +0530

    Init App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AboutComponent} from './about/about.component'
+import {AlertComponent} from './alert/alert.component'
+import {NavbarComponent} from './navbar/navbar.component'
+import {TextformComponent} from './textform/textform.component'
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+const appRoutes: Routes = [
+  {path: '', component: TextformComponent},
+  {path: 'about', component: AboutComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AlertComponent,
+    NavbarComponent,
+    TextformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
